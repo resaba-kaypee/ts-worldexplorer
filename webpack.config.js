ca039a80d63f3cb010fb7c63228d9cea3fbfa14a
kaypee <resaba.kaypee@gmail.com>
2021-01-18 23:10:01 +0700

    init setup with webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.ts",
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: path.join(__dirname, "src"),
+    compress: true,
+    port: 9000,
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "dist",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+};
