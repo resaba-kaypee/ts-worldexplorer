f655ddc3196f0b1da9e77baf07dc2b3f0d225449
kaypee <resaba.kaypee@gmail.com>
2021-02-15 18:57:22 +0700

    implement other display methods

--- src/views/world-explorer-view.ts
+++ src/views/world-explorer-view.ts
@@ -1,6 +1,7 @@
 import { Country } from "../domain";
 import { WorldExplorerView } from "./world-explorer-view.intf";
 import { ChartDetails } from "./chart-details.intf";
+import Chart = require("chart.js");
 
 export class WorldExplorerHTMLView implements WorldExplorerView {
   private readonly _countrySelect: HTMLSelectElement;
@@ -9,6 +10,8 @@ export class WorldExplorerHTMLView implements WorldExplorerView {
   private readonly _toYearSelect: HTMLSelectElement;
   private readonly _chartTypeSelect: HTMLSelectElement;
   private readonly _chartConfigurationForm: HTMLFormElement;
+  private readonly _canvas: HTMLCanvasElement;
+  private _chart?: Chart;
 
   constructor() {
     this._countrySelect = document.getElementById(
@@ -85,11 +88,45 @@ export class WorldExplorerHTMLView implements WorldExplorerView {
   }
 
   displayCountries(countries: Country[]): void {
-    // FIX ME implement
+    if (!countries) {
+      throw new Error("The list of countries to display must be provided!");
+    } else if (countries.length === 0) {
+      throw new Error("The list of countries cannot be empty!");
+    }
+
+    console.log("Displaying the countries");
+
+    let countriesOptions = "";
+
+    countries.forEach((country) => {
+      countriesOptions += `<option value="${country.id}">${country.name}</option>`;
+    });
+
+    this._countrySelect.innerHTML = countriesOptions;
   }
 
   displayYears(years: number[]): void {
-    // FIX ME implement
+    if (!years) {
+      throw new Error("The list of years must be provided!");
+    } else if (years.length === 0) {
+      throw new Error("The list of years cannot be empty!");
+    }
+
+    console.log("Displaying the years");
+
+    let fromYearOptions = "";
+    years.forEach((year) => {
+      fromYearOptions += `<option value="${year}">${year}</option>`;
+    });
+
+    // reverse order
+    let toYearOptions = "";
+    years.reverse().forEach((year) => {
+      toYearOptions += `<option value="${year}">${year}</option>`;
+    });
+
+    this._fromYearSelect.innerHTML = fromYearOptions;
+    this._toYearSelect.innerHTML = toYearOptions;
   }
 
   getChartFormDetails(): {
@@ -100,11 +137,135 @@ export class WorldExplorerHTMLView implements WorldExplorerView {
     toYear?: number;
     chartType?: string;
   } {
-    // FIX ME implement
-    return {};
+    if (this._chartConfigurationForm.checkValidity() === false) {
+      this._chartConfigurationForm.reportValidity();
+      return {
+        error: "The chart configuration form is invalid!",
+      };
+    }
+
+    // we check the validity of specific form fields
+    if (this._countrySelect.checkValidity() === false) {
+      this._countrySelect.reportValidity();
+      return {
+        error: "A country must be selected!",
+      };
+    }
+
+    if (this._indicatorSelect.checkValidity() === false) {
+      this._indicatorSelect.reportValidity();
+      return {
+        error: "An indicator must be selected!",
+      };
+    }
+
+    if (this._fromYearSelect.checkValidity() === false) {
+      this._fromYearSelect.reportValidity();
+      return {
+        error: "A start year must be selected!",
+      };
+    }
+
+    if (this._toYearSelect.checkValidity() === false) {
+      this._toYearSelect.reportValidity();
+      return {
+        error: "An end year must be selected!",
+      };
+    }
+
+    if (this._chartTypeSelect.checkValidity() === false) {
+      this._chartTypeSelect.reportValidity();
+      return {
+        error: "A chart type must be selected!",
+      };
+    }
+
+    const countryId: string = this._countrySelect.value;
+    const indicator: string = this._indicatorSelect.value;
+    const fromYear = Number.parseInt(this._fromYearSelect.value);
+    const toYear = Number.parseInt(this._toYearSelect.value);
+    const chartType: string = this._chartTypeSelect.value;
+
+    return {
+      countryId,
+      indicator,
+      fromYear,
+      toYear,
+      chartType,
+    };
   }
 
   displayChart(chartDetails: ChartDetails): void {
-    // FIX ME implement
+    if (!chartDetails) {
+      throw new Error("The chart details must be provided!");
+    }
+
+    const dataLabels: string[] = [];
+    const dataValues: number[] = [];
+
+    chartDetails.data.forEach((dataPoint) => {
+      dataLabels.push(dataPoint.date);
+      dataValues.push(dataPoint.value);
+    });
+
+    this._chart = new Chart(this._canvas, {
+      type: chartDetails.chartType,
+      data: {
+        labels: dataLabels,
+        datasets: [
+          {
+            label: chartDetails.dataLabel,
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(75,192,192,0.4)",
+            borderColor: "rgba(75,192,192,1)",
+            borderCapStyle: "butt",
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: "miter",
+            pointBorderColor: "rgba(75,192,192,1)",
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "rgba(75,192,192,1)",
+            pointHoverBorderColor: "rgba(220,220,220,1)",
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: dataValues,
+          },
+        ],
+      },
+      options: {
+        animation: {
+          animateRotate: true,
+          easing: "easeOutQuart",
+        },
+        responsive: true,
+        scales: {
+          xAxes: [
+            {
+              display: true,
+              scaleLabel: {
+                display: true,
+                labelString: chartDetails.yAxisLabel,
+              },
+            },
+          ],
+          yAxes: [
+            {
+              display: true,
+              scaleLabel: {
+                display: true,
+                labelString: chartDetails.yAxisLabel,
+              },
+            },
+          ],
+        },
+        title: {
+          display: true,
+          text: chartDetails.title,
+        },
+      },
+    });
   }
 }
