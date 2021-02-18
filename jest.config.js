a47d8c8443d11751cd6de700e936ceb2806e0d94
kaypee <resaba.kaypee@gmail.com>
2021-02-18 23:01:45 +0700

    setup testing async code

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  roots: ["<rootDir>/src"],
+  preset: "ts-jest",
+  testEnvironment: "node",
+  verbose: false,
+  collectCoverage: false,
+  globals: {
+    "ts-jest": {
+      // reference: https://kulshekhar.github.io/ts-jest/user/config
+    },
+  },
+  setupFiles: ["./src/jest.setup.ts"],
+};
